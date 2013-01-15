10229e94c88e21d75f791aff9a9432c5ae277707
Przemek Mroczek <przemek@motoprogrammer-linux.(none)>
2013-01-16 00:47:53 +0100

    Utworzyłem generator katalogów

--- fseg.sh
+++ fseg.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+# przenoszenie plików do wygenerowanego drzewa katalogów
+for x in *
+do
+    file=`date +%Y -r $x`
+    f_name=`date +%d/%m -r $x`
+    if [ -d ../$file ]; then  
+	`cp $x ../$file`;
+     else 
+        `mkdir ../$file/` 
+	`cp $x ../$file/`
+	
+    fi
+    
+done
