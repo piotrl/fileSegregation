c2b1fdf7658c05e30f80503c0b4eec22de78fa66
Piotr Lewandowski <grovman@gmail.com>
2013-01-16 21:00:35 +0100

    wprowadzona modularność skryptu + wstępna obsługę błędów

--- fseg.sh
+++ fseg.sh
@@ -1,16 +1,52 @@
 #!/bin/bash
 
-# przenoszenie plików do wygenerowanego drzewa katalogów
-for x in *
-do
-    file=`date +%Y -r $x`
-    f_name=`date +%d/%m -r $x`
-    if [ -d ../$file ]; then  
-	`cp $x ../$file`;
-     else 
-        `mkdir ../$file/` 
-	`cp $x ../$file/`
-	
-    fi
-    
-done
+# generating directories by cretion date of files
+function generate_dir {
+	dir=$1
+	if [ $# -ge 2 ]; then
+		new_dir=$2
+	else
+		new_dir='cache'
+	fi
+
+	mkdir $new_dir
+	cd $dir
+
+
+	for file in *
+	do
+		cdate_year=`date +%Y -r $file`
+		#f_name=`date +%d/%m -r $file`
+
+		if [ -d ../$new_dir/$cdate_year ]; then
+			cp $file ../$new_dir/$cdate_year
+		else
+			mkdir ../$new_dir/$cdate_year/
+			cp $file ../$new_dir/$cdate_year/
+		fi
+	done
+}
+
+# error handling
+function error {
+	code=$1
+
+	case $code in
+		0) 	echo $0': brak argumentow.'
+			echo 'Poprawna skladnia: `fseg.sh [KATALOG_ZRODLO] [KATALOG_DOCELOWY]`'
+			;;
+		1) 	echo $0': blad nr 1';; # for the future!
+		*) 	echo 'We like trains';;
+	esac
+}
+
+# *
+# * main()
+# * ------------------------------------------
+# *
+
+if [ $# -eq 0 ]; then
+	error 0
+else
+	generate_dir $1 $2
+fi
