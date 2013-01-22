9e1d2a76211aeec666b3963c390e70a7902fa61b
Piotr Lewandowski <grovman@gmail.com>
2013-01-22 01:37:06 +0100

    Docs && --help

--- fseg.sh
+++ fseg.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 function generate_dir {
+	# generate list of directories by files modification date
+	# and move/copy them right away
+	#
+	# $new_dir	assumes a value according to stdin
+	# 		 	if $2 isn't exist, then by default
+	#			get as a value, current directory.
+	# $dir 	 	name of current directory.
+	#			Read form the stdin (check the main section)
+	# $files	Stores list of files to segregate (with the path of each)
 
 	if [ $# -eq 2 ]; then
 		new_dir=$2
@@ -8,7 +17,7 @@ function generate_dir {
 		new_dir=$dir
 	fi
 
-	if [ reccur = true ]; then
+	if [ setting_reccur = true ]; then
 		files=`find $dir/* -type f`
 	else
 		files=`find $dir/* -maxdepth 0 -type f`
@@ -19,7 +28,7 @@ function generate_dir {
 		mdate_year=`date +%Y -r $file`
 		mdate_month=`date +%m -r $file`
 
-		if [ $downto_day = true ]; then
+		if [ $setting_day = true ]; then
 			mdate_day=`date +%d -r $file`
 		else
 			mdate_day=''
@@ -28,28 +37,45 @@ function generate_dir {
 		path=$new_dir/$mdate_year/$mdate_month/$mdate_day
 
 		if [ -d $path ]; then
-     		    $action $file $path
+     		    $setting_action $file $path
 		else
 		    mkdir -p $path
-		    $action $file $path
+		    $setting_action $file $path
 		fi
 	done
 }
 
 function backup {
+	# $new_dir	[ check in generate_dir ]
+
 	name='backup_'`date +%F`
 	cd $new_dir
 	tar -cvzf $name.tar.gz *
 }
 
+function help {
+	echo
+	echo 'Skladnia: `fseg.sh [KATALOG_ZRODLO] [KATALOG_DOCELOWY]`'
+	echo 'Jeśli nie podany [KATALOG_DOCELOWY], to segreguje wewnątrz [KATALOG_ZRODLO]'
+	echo
+	echo 'Argumenty nieobowiązkowe, wyłącznie krótkie opcje:'
+	echo '	-b 	spakowanie backupu posegregowanego katalogu (nazwa tworzona automatycznie)'
+	echo '	-d 	zawęża datę segregacji do dnia ostatniej modyfikacji pliku'
+	echo '	-m 	przenosi segregowane pliki przez `mv`'
+	echo '	-r 	uwzględnia podkatalogi podczas segregacji'
+	echo
+}
+
 function error {
+	# $code	store the number of error
+	# $option store the name of option (only in 405 err)
 
 	code=$1
 	option=$2
 
 	case $code in
 		0)	 echo $0': brak argumentow.'
-			 echo 'Poprawna skladnia: `fseg.sh [KATALOG_ZRODLO] [KATALOG_DOCELOWY]`'
+			 echo 'Spróbuj `fseg.sh --help` dla uzyskania informacji.'
 			 ;;
 		401) echo $0': Brak dostępu do pliku lub katalogu ';;
 		403) echo $0': Podany plik nie jest katalogiem';;
@@ -60,26 +86,27 @@ function error {
 	exit 0
 }
 
-# *
-# * main()
-# * ------------------------------------------
-# *
+#
+# main()
+# ------------------------------------------
+#
 
 if [ $# -eq 0 ]; then
 	error 0
 else
-	backup=false
-	action='cp'
-	downto_day=false
-	reccur=false
+	setting_backup=false
+	setting_action='cp'
+	setting_day=false
+	setting_reccur=false
 
 	while [ $# -gt 0 ]; do # options
 
 		case $1 in
-			-b) backup=true;;
-			-m) action='mv';;
-			-d) downto_day=true;;
-			-r) reccur=true;;
+			-b) setting_backup=true;;
+			-m) setting_action='mv';;
+			-d) setting_day=true;;
+			-r) setting_reccur=true;;
+			--help) help;;
 			--) shift
 				break
 				;;
@@ -100,8 +127,8 @@ else
 
 				generate_dir $dir $2   # we can do it, finally!
 
-				if [ $backup = true ]; then
-					backup $2
+				if [ $setting_backup = true ]; then
+					backup
 				fi
 			else
 				error 401
